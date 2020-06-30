1307494a94c969c14626c5603ea32b0499698656
Abdul Ahad <62215885+ahadabdulk@users.noreply.github.com>
2020-07-01 00:20:04 +0200

    Commiting the changes missed in previous commit

--- include/ModuleNetwork.h
+++ include/ModuleNetwork.h
@@ -10,7 +10,6 @@
 typedef std::shared_ptr<Module> Mptr;
 typedef std::list<Mptr> Mlist;
 typedef std::map<Mptr,Mlist> ModuleAdjList;
-typedef std::vector<std::string> OutString;
 typedef std::map<std::string,Mptr> ModulesMap;
 
 namespace std
@@ -32,10 +31,9 @@ public:
     static void updateModuleCount();
     static int getModuleCount();
 
-    //void connect(std::shared_ptr<Module> mod1, std::shared_ptr<Module> mod2);
     void connect(std::string mod1, std::string mod2);
     void addModule(std::shared_ptr<Module> mod);
-    void process(std::vector<std::string>);
+    void process(std::vector<std::string>& vec, std::vector<std::string>& outVec);
 
 private:
     ModuleNetwork()
@@ -47,7 +45,6 @@ private:
     static int            modCount;
 
     ModuleAdjList m_modAdjList;
-    OutString m_outString;
     ModulesMap m_modulesMap;
     Mptr m_startMod;
     bool m_isFirstModSet;
